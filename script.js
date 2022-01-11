eee9d3062e0d72d72dffa80bc7003281447be3d7
Hariz Halidi <hariz.halidi@gmail.com>
2022-01-11 10:07:33 +0800

    added drawing functionality using mouse

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const gridcontainer = document.querySelector('.gridcontainer');
-const n = 16;
+let isDrawing = false;
+const n = 30;
 
 for (let i = 0; i < n; i++) {
   gridcontainer.appendChild(document.createElement('div'));
@@ -8,5 +9,22 @@ for (let i = 0; i < n; i++) {
 gridcontainer.childNodes.forEach(rowcontainer => {
   for (let i = 0; i < n; i++) {
     rowcontainer.appendChild(document.createElement('div'));
-  }  
+  }
+});
+
+const cells = document.querySelectorAll('.gridcontainer > div > div');
+cells.forEach(cell => {
+  cell.addEventListener('mousedown', e => {
+    e.target.style.backgroundColor = 'black';
+    isDrawing = true;
+  });
+  cell.addEventListener('mouseenter', e => {
+    if (isDrawing) {
+      e.target.style.backgroundColor = 'black';
+    }
+  });
+});
+
+document.addEventListener('mouseup', () => {
+  isDrawing = false;
 });
